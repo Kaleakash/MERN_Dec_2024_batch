1859664c68835ea572119c3163d110ed0d3d79e1
Akash <akash300383@gmail.com>
2024-12-21 23:25:36 +0530

    done

---
+++ Phase 2/React JS Programs/product-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductOperation from './ProductOperation';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Product Management System!</h2>
+      <ProductOperation></ProductOperation>
+    </div>
+  );
+}
+
+export default App;
--- Phase 2/React JS Programs/react-component-communication/src/App.js
+++ Phase 2/React JS Programs/react-component-communication/src/App.js
@@ -1,12 +1,12 @@
 import Parent from "./Parent";
-
+import './App.css'
 
 
 function App() {
 
   return(
     <div>
-      <h2>Welcome to React JS Component Communication</h2>
+      <h2 className="MyTagClass">Welcome to React JS Component Communication</h2>
       <Parent></Parent>
     </div>
   )

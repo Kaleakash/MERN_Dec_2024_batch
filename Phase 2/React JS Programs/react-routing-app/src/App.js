6aab192a8e11d2bcf7439510869febbb1ca1a4b6
Akash <akash300383@gmail.com>
2025-01-11 20:54:24 +0530

    done

--- Phase 2/React JS Programs/react-routing-app/src/App.js
+++ Phase 2/React JS Programs/react-routing-app/src/App.js
@@ -4,6 +4,8 @@ import { Link, Route, Routes } from 'react-router-dom';
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
 import Feedback from './Feedback';
+import Login from './Login';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
@@ -14,14 +16,16 @@ function App() {
         <Link to="/about-us">About Us</Link> |
         <Link to="/contact-us">Contact Us</Link> |
         <Link to="/feedback">Feedback</Link> |
+        <Link to="/">Login</Link> |
       </div>
       
       <hr/>
       <Routes>
-      
-      <Route path="/about-us" element={<AboutUs></AboutUs>} />
-      <Route path="/contact-us" element={<ContactUs></ContactUs>} />
-      <Route path="/feedback" element={<Feedback></Feedback>} />
+        <Route path="/" element={<Login></Login>} />
+        <Route path="/about-us" element={<AboutUs></AboutUs>} />
+        <Route path="/contact-us" element={<ContactUs></ContactUs>} />
+        <Route path="/feedback" element={<Feedback></Feedback>} />
+        <Route path="/home" element={<Dashboard></Dashboard>} />
       </Routes>
     </div>
   );
--- Phase 2/React JS Programs/react-routing-app/src/Dashboard.js
+++ Phase 2/React JS Programs/react-routing-app/src/Dashboard.js
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
 
+let navigate = useNavigate();
+
+let logout = () => {
+    navigate("/");
+}
 
     return(
         <div>
             <h2>Welcome to Home Page</h2>
-
+            <input type="button" value="logout" onClick={logout}/>
         </div>
     )
 }

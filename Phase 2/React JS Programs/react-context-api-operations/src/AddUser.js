5f1767cbc78d77300ab1c108939e74d05bc8c774
Akash <akash300383@gmail.com>
2025-01-12 22:16:49 +0530

    done

--- Phase 2/React JS Programs/react-context-api-operations/src/AddUser.js
+++ Phase 2/React JS Programs/react-context-api-operations/src/AddUser.js
@@ -1,9 +1,31 @@
-function AddUser() {
+import { useContext, useState } from "react";
+import { UserContext } from "./Context";
 
+function AddUser() {
+let [name,setName]=useState("")
+let [age,setAge]=useState(0);
+
+//let {userOperation}= useContext(UserContext);
+let {users,setUsers}= useContext(UserContext);
+let addUser = (event)=> {
+    event.preventDefault();
+    let user = {name,age};      // convert to object 
+    //console.log(user);
+    //1st parameter is action type and 2nd parameter is payload ie object 
+    //userOperation("ADD",user);      // calling App component function from AddUser Component 
+    setUsers([...users,user]);  
+}
 
     return(
         <div>
             <h3>Add User</h3>
+            <form onSubmit= {addUser}>
+                <label>Name:</label>
+                <input type="text" name="name" onChange={(event)=>setName(event.target.value)} /><br /><br />
+                <label>Age:</label>
+                <input type="number" name="age" onChange={(event)=>setAge(event.target.value)} /><br /><br />
+                <input type="submit" value="Add User" /><br /><br />
+            </form>
         </div>
     )
 }
--- Phase 2/React JS Programs/react-context-api-operations/src/App.js
+++ Phase 2/React JS Programs/react-context-api-operations/src/App.js
@@ -8,13 +8,17 @@ function App() {
 
 let [users, setUsers] = useState([])
 
-let displayUser = ()=> {
-  console.log("Display User");
-}
+// let userOperation = (actionType,payload)=> {
+//   console.log("Parent component function called..");
+//   if(actionType=="ADD"){
+//     setUsers([...users,payload]);   //...users is previous state and payload is new state
+//     alert("User added successfully");
+//   }
 
+// }
 
   return (
-    <UserContext.Provider value={{users,displayUser,setUsers}}>
+    <UserContext.Provider value={{users,setUsers}}>
       <div className="App">
         <h2>Context API User Operations</h2>
         <AddUser />

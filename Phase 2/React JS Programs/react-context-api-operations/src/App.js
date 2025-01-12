5d9451518d4e3cc1c0ff0ff70275c2d8e788e437
Akash <akash300383@gmail.com>
2025-01-12 21:09:41 +0530

    done

---
+++ Phase 2/React JS Programs/react-context-api-operations/src/App.js
@@ -0,0 +1,26 @@
+import AddUser from './AddUser';
+import './App.css';
+import { UserContext } from './Context'
+import DisplayUser from './DisplayUser';
+
+function App() {
+
+let [users, setUsers] = useState([])
+
+let displayUser = ()=> {
+  console.log("Display User");
+}
+
+
+  return (
+    <UserContext.Provider value={{users,displayUser,setUsers}}>
+      <div className="App">
+        <h2>Context API User Operations</h2>
+        <AddUser />
+        <DisplayUser />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;

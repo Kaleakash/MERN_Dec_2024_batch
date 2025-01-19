6203f59528cce686106355ce4292dac6ca8c1828
Akash <akash300383@gmail.com>
2025-01-19 20:51:07 +0530

    done

---
+++ Phase 2/React JS Programs/react-testing-app/src/Counter.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+function Counter() {
+let [count, setCount] = useState(100);
+
+
+return(
+    <div>
+        <h3>Count:{count}</h3>
+    </div>
+)
+
+}
+
+export default Counter;
---
+++ Phase 2/React JS Programs/react-testing-app/src/Counter.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Counter from './Counter';
+
+test('Counter render test case', () => {
+render(<Counter />);
+
+const countElement = screen.getByText(/Count:/i);
+expect(countElement).toHaveTextContent('Count:0');
+
+});

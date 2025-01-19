713fe50d4119a6c45fd346044bfaa90d9b8c002b
Akash <akash300383@gmail.com>
2025-01-19 21:13:14 +0530

    done

--- Phase 2/React JS Programs/react-testing-app/src/Counter.js
+++ Phase 2/React JS Programs/react-testing-app/src/Counter.js
@@ -1,12 +1,19 @@
 import {useState} from 'react';
 
 function Counter() {
-let [count, setCount] = useState(100);
+let [count, setCount] = useState(10);
 
-
+let handleIncrement = () => {
+    setCount(count+1);
+}
+let handleDecrement = () => {
+    setCount(count-1);
+}
 return(
     <div>
-        <h3>Count:{count}</h3>
+        <h3>Count:{count}</h3><br/>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
     </div>
 )
 
--- Phase 2/React JS Programs/react-testing-app/src/Counter.test.js
+++ Phase 2/React JS Programs/react-testing-app/src/Counter.test.js
@@ -1,10 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import Counter from './Counter';
 
 test('Counter render test case', () => {
 render(<Counter />);
-
 const countElement = screen.getByText(/Count:/i);
-expect(countElement).toHaveTextContent('Count:0');
-
+expect(countElement).toHaveTextContent('Count:10');
 });
+
+test('Counter increment test case', () => {
+render(<Counter />);
+const incrementButton = screen.getByText(/Increment/i);
+act(() => {
+    incrementButton.click();
+})
+const countElement = screen.getByText(/Count:/i);
+expect(countElement).toHaveTextContent('Count:11');
+})
+
+test('Counter decrement test case', () => {
+    render(<Counter />);
+    const decrementButton = screen.getByText(/Decrement/i);
+    act(() => {
+        decrementButton.click();
+    })
+    const countElement = screen.getByText(/Count:/i);
+    expect(countElement).toHaveTextContent('Count:9');
+})

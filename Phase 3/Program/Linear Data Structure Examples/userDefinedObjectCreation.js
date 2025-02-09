730cdf239d3274c4d92c1167ad7b98644211c1a2
Akash <akash300383@gmail.com>
2025-02-09 20:29:09 +0530

    done

--- Phase 3/Program/Linear Data Structure Examples/userDefinedObjectCreation.js
+++ Phase 3/Program/Linear Data Structure Examples/userDefinedObjectCreation.js
@@ -29,22 +29,94 @@ console.log("age is "+emp1.age)
 
 
 // creating object in function style with parameter 
-function Employee(id,fname,age){
+// function Employee(id,fname,age){
     
-    this.id = id;      // this is a keyword which is used to refer current object
-    this.fname = fname;
-    this.age = age;          // it is consider as instance variable. 
+//     this.id = id;      // this is a keyword which is used to refer current object
+//     this.fname = fname;
+//     this.age = age;          // it is consider as instance variable. 
     
-    this.displayEmployee = function(){
-        console.log("Employee object in function style")
+//     this.displayEmployee = function(){
+//         console.log("Employee object in function style")
+//         console.log("id is "+this.id)
+//         console.log("name is "+this.fname)
+//         console.log("age is "+this.age)
+//     }
+
+// }
+// //Employee();     // it consider as normal function call 
+// let emp2 = new Employee(102,"Raj",34);      // it consider as object as function style 
+// emp2.displayEmployee();
+// let emp3 = new Employee(103,"Rajesh",36);      // it consider as object as function style 
+// emp3.displayEmployee();
+
+// creating object in class style with ES6 features (ES6 - ECMAScript 2015)
+// class Employee {
+//     id=100;
+//     fname="Ravi";
+//     age=34
+
+//     displayEmployee() {
+//         console.log("Employee object in class style")
+//         console.log("id is "+this.id)
+//         console.log("fname is "+this.fname)
+//         console.log("age is "+this.age)
+//     }
+// }
+
+// let emp2  = new Employee();
+// emp2.displayEmployee();
+// let emp3 = new Employee();
+// emp3.displayEmployee();
+// constructor is a type of special function which help to create the memory. 
+// creating the object using constructor with ES6 features (ES6 - ECMAScript 2015)
+class Employee {
+    constructor(id,fname,age){
+        this.id=id;
+        this.fname=fname;
+        this.age=age
+    }
+    displayEmployee() {
+        console.log("Employee object in class style")
         console.log("id is "+this.id)
-        console.log("name is "+this.fname)
+        console.log("fname is "+this.fname)
         console.log("age is "+this.age)
     }
-
 }
-//Employee();     // it consider as normal function call 
-let emp2 = new Employee(102,"Raj",34);      // it consider as object as function style 
+
+let emp2  = new Employee(103,"Raj",34);
 emp2.displayEmployee();
-let emp3 = new Employee(103,"Rajesh",36);      // it consider as object as function style 
+let emp3 = new Employee(104,"Rajesh",36);
 emp3.displayEmployee();
+
+// any user defined object 
+// 1. properties 2. methods
+// hair, eyes, height, weight, walk(), talk(), eat(), sleep(), run()
+class Person {
+    constructor(hair,eyes,height,weight){
+        this.hair=hair;
+        this.eyes=eyes;
+        this.height=height;
+        this.weight=weight
+    }
+    walk(){
+        console.log("walking")
+    }
+    talk(){
+        console.log("talking")
+    }
+    eat(){
+        console.log("eating")
+    }
+    sleep(){
+        console.log("sleeping")
+    }
+    run(){
+        console.log("running")
+    }
+}
+let p1 = new Person("black","black",5.6,60);
+p1.walk();
+p1.talk();
+p1.eat();
+p1.sleep();
+p1.run();

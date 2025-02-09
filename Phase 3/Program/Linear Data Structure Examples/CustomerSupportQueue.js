bf5144dfe65feec4f878dd68b3b8ff2c38453cf9
Akash <akash300383@gmail.com>
2025-02-09 22:54:00 +0530

    done

--- Phase 3/Program/Linear Data Structure Examples/CustomerSupportQueue.js
+++ Phase 3/Program/Linear Data Structure Examples/CustomerSupportQueue.js
@@ -1,10 +1,15 @@
 class CustomerSupportQueue {
     constructor() {
         this.queue = [];
+        this.MAX_REQUEST_IN_QUEUE=3;
     }
     enqueue(customerRequest) {          // adding 
+        if(this.queue.length<this.MAX_REQUEST_IN_QUEUE){
         this.queue.push(customerRequest);
         console.log("Customer request added to the queue."+customerRequest);
+        }else {
+            console.log("Customer request can't be added to the queue."+customerRequest);
+        }
     }
     dequeue() {          // removing
         if (this.isEmpty()) {
@@ -24,13 +29,13 @@ class CustomerSupportQueue {
         return this.isEmpty()?"No pending request ":this.queue[0];
     }
 }
-
 let customerSupportQueue = new CustomerSupportQueue();
 console.log("Any request present in queue "+(customerSupportQueue.isEmpty()?"No request yet in queue":"Yes"))
 console.log("Number of customer requests in the queue: " + customerSupportQueue.size())
-customerSupportQueue.enqueue("Customer 1 needs help with order Password reset");
+customerSupportQueue.enqueue("Customer 1 needs help with Account Password reset");
 customerSupportQueue.enqueue("Customer 2 needs help check order status");
 customerSupportQueue.enqueue("Customer 3 needs help with refund amount");
+customerSupportQueue.enqueue("Customer 4 needs help with create the account");
 console.log("Number of customer requests in the queue: " + customerSupportQueue.size())
 console.log("Any request present in queue "+(customerSupportQueue.isEmpty()?"No request yet in queue":"Yes, there are totally "+customerSupportQueue.size()+"request"))
 console.log("First request in queue is "+customerSupportQueue.peek())

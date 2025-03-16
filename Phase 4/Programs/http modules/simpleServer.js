bf0d108e808e38bb20077ff2aa228269814909f8
Akash <akash300383@gmail.com>
2025-03-16 19:54:21 +0530

    done

--- Phase 4/Programs/http modules/simpleServer.js
+++ Phase 4/Programs/http modules/simpleServer.js
@@ -8,7 +8,9 @@ console.log("module loaded...")
 let server = http.createServer((request,response)=> {
     response.end("Hello Client, Welcome to NodeJS Server");
 });
-
+// server is listening on port 3000
+// so whenever any client application send the request with url as http://localhost:3000 then this callback function will be called
+// and it will send the response as "Hello Client, Welcome to NodeJS Server" to the client application
 server.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });

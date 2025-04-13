f8144cf3f65b9b0041ddef67980bae9c58e39e65
Akash <akash300383@gmail.com>
2025-04-13 19:55:36 +0530

    done

---
+++ Phase 4/Programs/consuming rest api in node js application/consume product rest api using axios/app.js
@@ -0,0 +1,11 @@
+let axios = require("axios");
+
+let loadFakeData = async()=> {
+let response = await axios.get("https://fakestoreapi.com/products")
+let products = await response.data;
+console.log(products);
+}
+
+
+
+loadFakeData();
